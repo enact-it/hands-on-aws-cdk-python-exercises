027db4aeb6473f51c257bb003dc413d3d175cd48
Ben de Haan <53901866+bendehaan@users.noreply.github.com>
2024-01-14 14:32:59 +0100

    fix: precommit fixes

--- 01-my-first-stack/final/myfirst/my_first_stack.py
+++ 01-my-first-stack/final/myfirst/my_first_stack.py
@@ -2,7 +2,7 @@ from constructs import Construct
 from aws_cdk import (
     Duration,
     Stack,
-    aws_iam as iam,
+    # aws_iam as iam,
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
@@ -10,17 +10,15 @@ from aws_cdk import (
 
 
 class MyFirstStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         queue = sqs.Queue(
-            self, "MyFirstQueue",
+            self,
+            "MyFirstQueue",
             visibility_timeout=Duration.seconds(300),
         )
 
-        topic = sns.Topic(
-            self, "MyFirstTopic"
-        )
+        topic = sns.Topic(self, "MyFirstTopic")
 
         topic.add_subscription(subs.SqsSubscription(queue))
