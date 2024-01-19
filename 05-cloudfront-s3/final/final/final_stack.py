bb00a66f8185259b82ae723d020566ddb4a47699
Ben de Haan <53901866+bendehaan@users.noreply.github.com>
2024-01-19 22:54:41 +0100

    feat: add lab 05

---
+++ 05-cloudfront-s3/final/final/final_stack.py
@@ -0,0 +1,56 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_s3 as s3,
+    aws_s3_assets as s3_assets,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as origins,
+    RemovalPolicy,
+    CfnOutput,
+)
+from constructs import Construct
+
+
+class FinalStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # TODO create a Bucket
+        bucket = s3.Bucket(
+            self,
+            "Bucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+            # Add bucket asset
+        )
+
+        # TODO Add index.html s3 asset
+        s3_assets.Asset(
+            self,
+            "Asset",
+            path="../files/index.html",
+        )
+
+        # TODO Add CloudFront origin access identity
+        origin_access_identity = cloudfront.OriginAccessIdentity(
+            self,
+            "OriginAccessIdentity",
+        )
+
+        # TODO Grant the origin access identity read permissions on the bucket
+        bucket.grant_read(origin_access_identity)
+
+        # TODO Create a CloudFront distribution with the bucket as an origin
+        distribution = cloudfront.Distribution(
+            self,
+            "Distribution",
+            default_root_object="index.html",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3Origin(
+                    bucket, origin_access_identity=origin_access_identity
+                )
+            ),
+        )
+
+        # TODO create a CfnOutput for the distribution domain name
+        CfnOutput(self, "DistributionDomainName", value=distribution.domain_name)
---
+++ 05-cloudfront-s3/starter/starter/final_stack.py
@@ -0,0 +1,29 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    # TODO uncomment
+    # aws_s3 as s3,
+    # aws_s3_assets as s3_assets,
+    # aws_cloudfront as cloudfront,
+    # aws_cloudfront_origins as origins,
+    # RemovalPolicy,
+    # CfnOutput
+)
+from constructs import Construct
+
+
+class FinalStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # TODO create a Bucket
+
+        # TODO Add index.html s3 asset
+
+        # TODO Add CloudFront origin access identity
+
+        # TODO Grant the origin access identity read permissions on the bucket
+
+        # TODO Create a CloudFront distribution with the bucket as an origin
+
+        # TODO create a CfnOutput for the distribution domain name
